670bd0ee6727368bc96b68cb44b628c110afa0fc
ehz0ah <haozhe4547@gmail.com>
2024-07-14 00:19:32 +0800

    First revision

---
+++ src/GetGradeCap.java
@@ -0,0 +1,30 @@
+public class GetGradeCap {
+    public static double getGradeCap(String grade) {
+        double cap = 0;
+        switch (grade) {
+            case "A+":
+            case "A":
+                cap = 5.0;
+                break;
+            case "A-":
+                cap = 4.5;
+                break;
+            case "B+":
+                cap = 4.0;
+                break;
+            case "B":
+                cap = 3.5;
+                break;
+            case "B-":
+                cap = 3.0;
+                break;
+            default:
+        }
+        return cap;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("A+: " + getGradeCap("A+"));
+        System.out.println("B : " + getGradeCap("B"));
+    }
+}
---
+++ src/GradeHelper.java
@@ -0,0 +1,31 @@
+public class GradeHelper {
+    public static void main(String[] args) {
+        String grade = args[0];
+        double gpa;
+        switch (grade) {
+            case "A+":
+            case "A":
+                gpa = 5.0;
+                break;
+            case "A-":
+                gpa = 4.5;
+                break;
+            case "B+":
+                gpa = 4.0;
+                break;
+            case "B":
+                gpa = 3.5;
+                break;
+            case "B-":
+                gpa = 3.0;
+                break;
+            case "C":
+                gpa = 2.5;
+                break;
+            default:
+                gpa = 0;
+                break;
+        }
+        System.out.println("CAP for grade " + grade + " is " + gpa);
+    }
+}
